25d94d38d380eb8e05b26ad1b0ccfdca36afbee3
Tushar Goyal <tusharjma8@gmail.com>
2019-10-03 12:26:11 +0530

    check vowel or consonant

---
+++ GCC/check_vowel.cpp
@@ -0,0 +1,45 @@
+//check vowel or consonant with switch statement 
+#include<iostream>
+using namespace std;
+int main(){
+	
+	char x;
+	cin>>x;
+
+	switch(x){
+
+		case 'a':cout<<"character is vowel"<<endl;
+				break;
+
+		case 'e':cout<<"character is vowel"<<endl;
+				break;
+
+		case 'i':cout<<"character is vowel"<<endl;
+				break;
+
+		case 'o':cout<<"character is vowel"<<endl;
+				break;
+
+		case 'u':cout<<"character is vowel"<<endl;
+				break;
+
+		case 'A':cout<<"character is vowel"<<endl;
+				break;
+
+		case 'E':cout<<"character is vowel"<<endl;
+				break;
+
+		case 'I':cout<<"character is vowel"<<endl;
+				break;
+
+		case 'O':cout<<"character is vowel"<<endl;
+				break;
+
+		case 'U':cout<<"character is vowel"<<endl;
+				break;
+
+		default:cout<<"character is consonant"<<endl;
+				break;
+
+	}
+}
